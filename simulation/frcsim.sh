8344111cd93c21dcb05ba431e044f12ba684fcdb
peter mitrano <mitranopeter@gmail.com>
2015-06-02 11:54:56 -0700

    added temporary frcsim.bat file

--- simulation/frcsim.sh
+++ simulation/frcsim.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export GAZEBO_PLUGIN_PATH="${GAZEBO_PLUGIN_PATH}:/usr/lib/frcsim/plugins"
-export GAZEBO_MODEL_PATH="${GAZEBO_MODEL_PATH}:/usr/share/frcsim/models"
-export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:/usr/lib/frcsim/plugins"
+export GAZEBO_PLUGIN_PATH="${GAZEBO_PLUGIN_PATH}:~/wpilib/simulation/plugins"
+export GAZEBO_MODEL_PATH="${GAZEBO_MODEL_PATH}:~/wpilib/simulation/models"
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:~/wpilib/simulation/plugins"
 gazebo $@
