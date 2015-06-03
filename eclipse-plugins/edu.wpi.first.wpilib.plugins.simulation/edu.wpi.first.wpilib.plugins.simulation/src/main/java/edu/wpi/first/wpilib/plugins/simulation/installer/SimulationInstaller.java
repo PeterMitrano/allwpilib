8da355152f492de838f89cfe01f30c03ed5683ce
peter mitrano <mitranopeter@gmail.com>
2015-06-02 19:59:28 -0700

    more mucking around with simulation plugin

---
+++ eclipse-plugins/edu.wpi.first.wpilib.plugins.simulation/edu.wpi.first.wpilib.plugins.simulation/src/main/java/edu/wpi/first/wpilib/plugins/simulation/installer/SimulationInstaller.java
@@ -0,0 +1,39 @@
+package edu.wpi.first.wpilib.plugins.simulation.installer;
+
+import java.io.InputStream;
+
+import org.eclipse.jface.preference.IPreferenceStore;
+
+import edu.wpi.first.wpilib.plugins.core.WPILibCore;
+import edu.wpi.first.wpilib.plugins.core.installer.AbstractInstaller;
+import edu.wpi.first.wpilib.plugins.simulation.WPILibSimulationPlugin;
+import edu.wpi.first.wpilib.plugins.simulation.preferences.PreferenceConstants;
+
+
+public class SimulationInstaller extends AbstractInstaller {
+
+	public SimulationInstaller(String version) {
+		super(version, WPILibSimulationPlugin.getDefault().getPreferenceStore().getString(PreferenceConstants.LIBRARY_INSTALLED), WPILibCore.getDefault().getWPILibBaseDir() + "/simulation");
+		WPILibSimulationPlugin.logInfo("\n\n HELLO FROM SIMULATION!!! \n\n");
+	}
+	
+	
+	@Override
+	protected String getFeatureName() {
+		return "simulation";
+	}
+
+	@Override
+	protected void updateInstalledVersion(String version) {
+		IPreferenceStore prefs = WPILibSimulationPlugin.getDefault().getPreferenceStore();
+		prefs.setValue(PreferenceConstants.LIBRARY_INSTALLED, version);
+		
+	}
+
+	@Override
+	protected InputStream getInstallResourceStream() {
+		return SimulationInstaller.class.getResourceAsStream("/resources/simulation.zip");
+	}
+}
+
+
---
+++ eclipse-plugins/edu.wpi.first.wpilib.plugins.simulation/edu.wpi.first.wpilib.plugins.simulation/src/main/java/edu/wpi/first/wpilib/plugins/simulation/preferences/PreferenceConstants.java
@@ -0,0 +1,8 @@
+package edu.wpi.first.wpilib.plugins.simulation.preferences;
+
+/**
+ * Constant definitions for plug-in preferences
+ */
+public class PreferenceConstants {
+	public static final String LIBRARY_INSTALLED = "libraryVersion_current";
+}
