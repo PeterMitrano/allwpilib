b6357182bd7b548cb039448f6b98fb1e3b28de7e
peter mitrano <mitranopeter@gmail.com>
2015-07-06 15:53:34 -0700

    removed detritus

--- eclipse-plugins/edu.wpi.first.wpilib.plugins.simulation/edu.wpi.first.wpilib.plugins.simulation/src/main/java/edu/wpi/first/wpilib/plugins/simulation/preferences/PreferenceConstants.java
+++
@@ -1,8 +0,0 @@
-package edu.wpi.first.wpilib.plugins.simulation.preferences;
-
-/**
- * Constant definitions for plug-in preferences
- */
-public class PreferenceConstants {
-	public static final String LIBRARY_INSTALLED = "libraryVersion_current";
-}
