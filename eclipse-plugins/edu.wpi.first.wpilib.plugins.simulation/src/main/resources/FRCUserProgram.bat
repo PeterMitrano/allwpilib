52194b39153fec37dd430907e1fb41e29b86096a
peter mitrano <mitranopeter@gmail.com>
2015-08-06 12:23:54 -0400

    Fixed java scripts and java build script for win

--- eclipse-plugins/edu.wpi.first.wpilib.plugins.simulation/src/main/resources/FRCUserProgram.bat
+++ eclipse-plugins/edu.wpi.first.wpilib.plugins.simulation/src/main/resources/FRCUserProgram.bat
@@ -1,5 +1,7 @@
-#first arg is jar file
-#second arg is path to sim/lib folder
-#third arg is things to append to LD_LIBRARY_PATH
-set LD_LIBRARY_PATH=$3
-java -Djava.library.path=$2 -jar $1
+@rem first arg is jar file
+@rem second arg is path to sim/lib folder
+@rem third arg is things to append to LD_LIBRARY_PATH
+@set GAZEBO_MASTER_URI="%4"
+@set GAZEBO_IP="%5"
+@set PATH="%3";%PATH%
+java -Djava.library.path="%2" -jar "%1"
