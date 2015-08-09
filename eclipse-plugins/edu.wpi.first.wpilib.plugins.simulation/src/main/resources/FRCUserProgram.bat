1a9c8453763084386f8135f5ea2b2d427b6ca8c1
peter mitrano <mitranopeter@gmail.com>
2015-08-08 23:19:44 -0400

    working on shell scripts for launching simulation
    this allows for environment variables to be set
    Ideally, this should be done in ant, but it may not be possible

--- eclipse-plugins/edu.wpi.first.wpilib.plugins.simulation/src/main/resources/FRCUserProgram.bat
+++ eclipse-plugins/edu.wpi.first.wpilib.plugins.simulation/src/main/resources/FRCUserProgram.bat
@@ -1,7 +1,6 @@
-@rem first arg is jar file
-@rem second arg is path to sim/lib folder
-@rem third arg is things to append to LD_LIBRARY_PATH
-@set GAZEBO_MASTER_URI="%4"
-@set GAZEBO_IP="%5"
-@set PATH="%3";%PATH%
-java -Djava.library.path="%2" -jar "%1"
+@set GAZEBO_MASTER_URI=%2
+@set GAZEBO_IP=%3
+cmd /k %home%\wpilib\simulation\win_addpath.bat
+@echo PATH = %PATH%
+@rem first arg is executable
+%1
--- eclipse-plugins/edu.wpi.first.wpilib.plugins.simulation/src/main/resources/FRCUserProgram.sh
+++ eclipse-plugins/edu.wpi.first.wpilib.plugins.simulation/src/main/resources/FRCUserProgram.sh
@@ -1,7 +1,5 @@
-#first arg is jar file
-#second arg is path to sim/lib folder
-#third arg is things to append to LD_LIBRARY_PATH
-export GAZEBO_MASTER_URI=$4
-export GAZEBO_IP=$5
-export LD_LIBRARY_PATH=$3
-java -Djava.library.path=$2 -jar $1
+export GAZEBO_MASTER_URI=$2
+export GAZEBO_IP=$3
+export LD_LIBRARY_PATH=$4:$5:$LD_LIBRARY_PATH
+#first arg is executable
+$1
